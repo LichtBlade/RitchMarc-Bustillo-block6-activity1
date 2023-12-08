43076bfcacd48646abe75381c5e6d53af8460f42
LichtBlade <risi.bustillo.up@phinmaed.com>
2023-12-08 15:45:08 +0800

    fourth commit

--- RitchMarcBustilloBlock6Quiz/app/src/main/java/com/bustillo/ritchmarc/p1/quiz/MainActivity.kt
+++ RitchMarcBustilloBlock6Quiz/app/src/main/java/com/bustillo/ritchmarc/p1/quiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bustillo.ritchmarc.p1.quiz
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,12 +10,14 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     private lateinit var tvResult: TextView
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnPlus = findViewById<Button>(R.id.btn_plus)
         val btnMinus = findViewById<Button>(R.id.btn_minus)
+        val btnMulti = findViewById<Button>(R.id.btn_multi)
         val firstNumber = findViewById<EditText>(R.id.et_firstNumber)
         val secondNumber = findViewById<EditText>(R.id.et_secondNumber)
         tvResult = findViewById(R.id.tv_result)
@@ -30,6 +33,12 @@ class MainActivity : AppCompatActivity() {
             val num2 = secondNumber.text.toString().toIntOrNull() ?: 0
             subtraction(num1,num2)
         }
+
+        btnMulti.setOnClickListener {
+            val num1 = firstNumber.text.toString().toIntOrNull() ?: 0
+            val num2 = secondNumber.text.toString().toIntOrNull() ?: 0
+            multi(num1,num2)
+        }
     }
 
     private fun addition(first: Int, second: Int) {
@@ -41,4 +50,10 @@ class MainActivity : AppCompatActivity() {
         val added = first - second
         tvResult.text = added.toString()
     }
+
+    private fun multi(first: Int, second: Int) {
+        val added = first * second
+        tvResult.text = added.toString()
+    }
+
 }
