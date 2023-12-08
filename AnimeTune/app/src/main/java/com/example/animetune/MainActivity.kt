fb7d6436086f5025873dc85115a8060904cfff19
LichtBlade <risi.bustillo.up@phinmaed.com>
2023-12-08 14:42:34 +0800

    first commit

--- AnimeTune/app/src/main/java/com/example/animetune/MainActivity.kt
+++ AnimeTune/app/src/main/java/com/example/animetune/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.animetune
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.animetune.databinding.ActivityMainBinding
+import com.example.animetune.first_time_user.StartApp
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val intent = Intent(this, StartApp::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
